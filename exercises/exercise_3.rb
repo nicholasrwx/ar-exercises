119f92bc7958725ee9caf4cfbc24879b3c738116
Nicholas Wiebe <digitaleistence18@gmail.com>
2021-03-04 23:52:16 +0000

    destroying a store in DB

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,38 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+class Business
+
+  attr_accessor :store3
+
+  def initialize
+      store = Store.new
+  end
+
+  store3 = Store.find_by("id = 3")
+
+  puts store3.name
+  
+  store3.destroy
+  
+  puts Store.count
+
+end
+
+
+
+
+
+
+# 1. Load the third store (into `@store3`) as you did the other two before.
+# 2. Using Active Record's `destroy` method, delete the store from the database.
+# 3. Verify that the store has been deleted by again outputting (`puts`ing) the `count` (as you did in Exercise 1.)
+
+
+
+
+
+
+
+
