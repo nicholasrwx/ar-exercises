2130a18d0e5606c8d5a0f393c61c5476606fcb10
Nicholas Wiebe <digitaleistence18@gmail.com>
2021-03-04 09:30:33 +0000

    Update Store1 in DB

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,18 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+class Business
+
+  attr_accessor :store1, :store2
+
+  def initialize
+      store = Store.new
+  end
+
+  store1 = Store.find_by("id = 1")
+  store2 = Store.find_by("id = 2")
+
+  store1.name = "Ruby"
+  store1.save
+
+end
