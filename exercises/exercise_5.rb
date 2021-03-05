1232c5d3dd8976364bdd84629be9f63223f996d9
Nicholas Wiebe <digitaleistence18@gmail.com>
2021-03-05 02:25:36 +0000

    Use specific methods to find specific values

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,18 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+
+value = Store.sum("Annual_revenue")
+puts value
+
+value = Store.average("Annual_revenue")
+puts value
+
+value = Store.where("Annual_revenue < ?", 1000000).size()
+puts value
+
+
+# 1. Output the total revenue for the entire company (all stores), using Active Record's `.sum` calculation method.
+# 2. On the next line, also output the average annual revenue for all stores.
+# 3. Output the number of stores that are generating $1M or more in annual sales. **Hint:** Chain together `where` and `size` (or `count`) Active Record methods.
